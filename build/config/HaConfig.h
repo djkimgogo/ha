b88f6991810ee6b8ab9e91c0f8dd9a799ddbaa4d
DJ.Kim <djkimgogo@gmail.com>
2017-12-26 23:27:29 -0800

    - add .gitignore with deleted build directory

--- build/config/HaConfig.h
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) HiAi, Inc - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by DJ Kim <djkimgo@gmail.com>, September 2017
- */
-// the configured options and settings for Tutorial
-#define HA_VERSION_MAJOR 0
-#define HA_VERSION_MINOR 1
